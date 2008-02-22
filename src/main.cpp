ecc9ab05aa35c1da419e4d6a5c7ce86d9c442113
Tj <tj.ferrell@gmail.com>
2008-02-22 18:08:05 +0000

    Added Xcode project Added src directory with main.cpp Ignored build directory and user-specific Xcode files

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ *  main.cpp
+ *  Crab Battle
+ *
+ *  Created by TJ Ferrell on 2/22/08.
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
