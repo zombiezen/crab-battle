6c0e4ef3030bc9ee0635b677e56f7fb9d4d7a64d
rlight2 <rlight2@8ee8f71a-c246-0410-bbc3-1bc83876a718>
2008-02-23 21:17:27 +0000

    Updated Xcode project to actually work with SDL Changed main.cpp to do something SDL-related Added precompiled header

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -6,12 +6,68 @@
  *  $Id$
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 #include <iostream>
+#include <cstring>
+
+#include "SDL.h"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+// Our wonderful main function
+// The extern "C"
+extern "C" int main(int argc, char *argv[])
 {
-    cout << "Hello, World!" << endl;
-    return 0;
+    Uint32 initflags = SDL_INIT_VIDEO;  /* See documentation for details */
+    SDL_Surface *screen;
+    Uint8 video_bpp = 0;
+    Uint32 videoflags = SDL_SWSURFACE;
+    int done;
+    SDL_Event event;
+    
+    /* Initialize the SDL library */
+    if (SDL_Init(initflags) < 0)
+    {
+        cerr << "Couldn't initialize SDL: " << SDL_GetError() << endl;
+        exit(1);
+    }
+    
+    /* Set 640x480 video mode */
+    screen = SDL_SetVideoMode(640, 480, video_bpp, videoflags);
+    if (screen == NULL)
+    {
+        cerr << "Couldn't set 640x480x" << video_bpp << " video mode: " << SDL_GetError() << endl;
+        SDL_Quit();
+        exit(2);
+    }
+    
+    done = 0;
+    while (!done)
+    {
+        /* Check for events */
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type) {
+                    
+                case SDL_MOUSEMOTION:
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    break;
+                case SDL_KEYDOWN:
+                    /* Any keypress quits the app... */
+                case SDL_QUIT:
+                    done = 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    
+    /* Clean up the SDL library */
+    SDL_Quit();
+    return(0);
 }
