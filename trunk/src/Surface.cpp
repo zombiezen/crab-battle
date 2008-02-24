23c5d0737aba70c4cee904283b53dcf22716c3a4
rlight2 <rlight2@8ee8f71a-c246-0410-bbc3-1bc83876a718>
2008-02-24 01:39:18 +0000

    Added #ifdef header check Added SDL_Surface convert from initializer Added GetRect, Fill, and Update methods Added getVideoSurface function

--- trunk/src/Surface.cpp
+++ trunk/src/Surface.cpp
@@ -10,6 +10,16 @@
 #include <SDL_image/SDL_image.h>
 
 using CrabBattle::Surface;
+using CrabBattle::Rect;
+
+static Surface *_gVideoSurface = NULL;
+
+Surface *getVideoSurface(void)
+{
+    if (_gVideoSurface == NULL)
+        _gVideoSurface = new Surface(SDL_GetVideoSurface());
+    return _gVideoSurface;
+}
 
 Surface::Surface(unsigned int w, unsigned int h)
 {
@@ -27,6 +37,12 @@ Surface::Surface(Uint32 flags, unsigned int w, unsigned int h, unsigned int bps)
     surface = SDL_CreateRGBSurface(flags, w, h, bps, 0, 0, 0, 0);
 }
 
+Surface::Surface(SDL_Surface *surf)
+{
+    surface = surf;
+    surface->refcount++; // We don't own the surface, so we don't want to delete it on object deletion
+}
+
 SDL_Surface *Surface::GetSurface(void)
 {
     return surface;
@@ -42,8 +58,25 @@ unsigned int Surface::GetHeight(void)
     return surface->h;
 }
 
+Rect Surface::GetRect(void)
+{
+    return Rect(0, 0, GetWidth(), GetHeight());
+}
+
+void Surface::Fill(Rect dest, unsigned int r, unsigned int g, unsigned int b)
+{
+    Uint32 color = SDL_MapRGB(surface->format, r, g, b);
+    SDL_Rect actualRect = dest.ConvertToSDLRect();
+    SDL_FillRect(surface, &actualRect, color);
+    // TODO: If result of SDL_FillRect is -1, raise an error
+}
+
+void Surface::Update(Rect region)
+{
+    SDL_UpdateRect(surface, region.GetX(), region.GetY(), region.GetWidth(), region.GetHeight());
+}
+
 Surface::~Surface(void)
 {
     SDL_FreeSurface(surface);
-    delete surface;
 }
