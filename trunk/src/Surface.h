c9a2d8b4a6d38fbaaadf65520370b175278caf13
rlight2 <rlight2@8ee8f71a-c246-0410-bbc3-1bc83876a718>
2008-02-24 01:39:27 +0000

    Added #ifdef header check Added SDL_Surface convert from initializer Added GetRect, Fill, and Update methods Added getVideoSurface function

--- trunk/src/Surface.h
+++ trunk/src/Surface.h
@@ -8,6 +8,11 @@
 
 #include "SDL.h"
 
+#include "Rect.h"
+
+#ifndef _CRAB_BATTLE_SURFACE_H_
+#define _CRAB_BATTLE_SURFACE_H_
+
 namespace CrabBattle
 {
     class Surface
@@ -18,9 +23,17 @@ namespace CrabBattle
         Surface(unsigned int, unsigned int);    // Create from (w, h)
         Surface(const char *);                  // Load from file
         Surface(Uint32, unsigned int, unsigned int, unsigned int); // flags, w, h, bitsPerPixel
+        Surface(SDL_Surface *);
         SDL_Surface *GetSurface(void);
         unsigned int GetWidth(void);
         unsigned int GetHeight(void);
+        Rect GetRect(void);
+        void Fill(Rect, unsigned int, unsigned int, unsigned int);
+        void Update(Rect);
         ~Surface(void);
     };
+    
+    Surface *getVideoSurface(void);
 }
+
+#endif
