415d1a31b7ded9e2a699ae7d1b7d9450f6402477
rlight2 <rlight2@8ee8f71a-c246-0410-bbc3-1bc83876a718>
2008-03-13 17:57:18 +0000

    Added preprocessor option for refcount debugging

--- branches/refcount/src/BaseObject.cpp
+++ branches/refcount/src/BaseObject.cpp
@@ -6,29 +6,42 @@
  *  $Id$
  */
 
+#ifdef DEBUG_BASEOBJECT_REFCOUNT
 #include <iostream>
+#endif
 #include "BaseObject.h"
 
 using CrabBattle::BaseObject;
 
 BaseObject::BaseObject(void)
 {
-    using std::cout;
-    using std::endl;
     _refCount = 1;
-    cout << "BaseObject::BaseObject()" << endl;
+    #ifdef DEBUG_BASEOBJECT_REFCOUNT
+    std::cout << "OBJECT: CREATED" << std::endl;
+    #endif
 }
 
 void BaseObject::AddRef(void)
 {
     _refCount++;
+    #ifdef DEBUG_BASEOBJECT_REFCOUNT
+    std::cout << "OBJECT: RETAINED" << std::endl;
+    #endif
 }
 
 void BaseObject::DelRef(void)
 {
     _refCount--;
+    #ifdef DEBUG_BASEOBJECT_REFCOUNT
+    std::cout << "OBJECT: RELEASED" << std::endl;
+    #endif
     if (_refCount == 0)
+    {
+        #ifdef DEBUG_BASEOBJECT_REFCOUNT
+        std::cout << "OBJECT: DEALLOCATED" << std::endl;
+        #endif
         delete this;
+    }
 }
 
 BaseObject::~BaseObject(void)
