1d5bc5ac53937e7f842a54092644d813cd4ee01b
rlight2 <rlight2@8ee8f71a-c246-0410-bbc3-1bc83876a718>
2008-03-27 01:37:46 +0000

    Fixed Issue 15

--- trunk/src/constants.h
+++ trunk/src/constants.h
@@ -6,11 +6,19 @@
  *  $Id$
  */
 
+#if !defined(__WIN32__) && !defined(WINDOWS)
+#include <sys/param.h> // for MAXPATHLEN
+#endif
+
 #ifndef _CRAB_BATTLE_CONSTANTS_H_
 #define _CRAB_BATTLE_CONSTANTS_H_
 
 const unsigned short kUpdateRate = 33; // in ms
 const int kScreenWidth = 640; // in pixels
 const int kScreenHeight = 480; // in pixels
+
+#ifndef MAXPATHLEN
+#define MAXPATHLEN 512 // a reasonable assumption, I hope :D
+#endif
 
 #endif
