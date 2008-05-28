971c0cea51c291bc2fef694185b7da41853a6f87
rlight2 <rlight2@8ee8f71a-c246-0410-bbc3-1bc83876a718>
2008-05-28 22:39:44 +0000

    Fixed Subversion keywords on PlayerSelect.{h,cpp}

--- trunk/src/PlayerSelect.h
+++ trunk/src/PlayerSelect.h
@@ -3,16 +3,15 @@
  *  Crab Battle
  *
  *  Created by TJ Ferrell on 5/27/08.
- *  Copyright 2008 AOC. All rights reserved.
- *
+ *  $Id$
  */
 
 #include "SDL.h"
 #include "Surface.h"
 #include "State.h"
 
-#ifndef _CRAB_BATTLE_PLAYERSELECTION_H_
-#define _CRAB_BATTLE_PLAYERSELECTION_H_
+#ifndef _CRAB_BATTLE_PLAYERSELECT_H_
+#define _CRAB_BATTLE_PLAYERSELECT_H_
 
 namespace CrabBattle
 {
